0cdc571433b26b90a05ab8215681837f8e46ef1f
Studio_AP <32787289+Hydrashok@users.noreply.github.com>
2026-08-17 16:59:08 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Offline support. Bump CACHE when you ship changes, or returning
    users keep getting the old version out of their cache. */
 
-const CACHE = "art-helper-v18";
+const CACHE = "art-helper-v19";
 
 const SHELL = [
   "./",
